e688321baa161ee1c754b6b92d25a148e156a545
Nithin636342 <97435836+Nithin636342@users.noreply.github.com>
2022-02-04 04:24:45 +0000

    good

---
+++ p2final.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter three number\n");
+  scanf("%d",&a);
+  return a;
+}
+int cmp(int a,int b,int c)
+{
+  if((a>b)&&(a>c))
+  return a;
+  else if((b>a)&&(b>c))
+  return b;
+  else c;
+}
+int  output(int big)
+{
+  printf("biggest number is %d",big);
+}
+int main()
+{
+  int x,y,z,biggest;
+  x=input();
+  y=input();
+  z=input();
+  biggest=cmp(x,y,z);
+  output(biggest);
+  return 0;
+}
